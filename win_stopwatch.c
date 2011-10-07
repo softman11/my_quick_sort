e1f7e215ea3bb9b221de4b855c963f28c375342f
unknown <Administrator@.(none)>
2011-10-07 09:58:49 +0800

    add win_stopwatch.h and win_stopwatch.c

---
+++ win_stopwatch.c
@@ -0,0 +1,31 @@
+#include <windows.h>   
+#include "win_stopwatch.h"   
+LARGE_INTEGER beginticks;  
+LARGE_INTEGER endticks ;  
+LARGE_INTEGER  frequency;
+int init_flag=0;
+void init_win_stopwatch()  
+{  
+    
+  beginticks.QuadPart=0;  
+  endticks.QuadPart=0;  
+  frequency.QuadPart=0;  
+  QueryPerformanceFrequency(&frequency);  
+}  
+void start_win_stopwatch()  
+{  
+      
+	if(!init_flag){init_win_stopwatch();init_flag=1;}
+	QueryPerformanceCounter(&beginticks);  
+}  
+void end_win_stopwatch()  
+{  
+     
+   QueryPerformanceCounter(&endticks);  
+}  
+double get_win_stopwatch_ms()  
+{  
+    unsigned long long cost=(unsigned long long)(endticks.QuadPart-beginticks.QuadPart);  
+    double millsecond=(double)cost*1000.0/(double)frequency.QuadPart;  
+    return millsecond;  
+} 
---
+++ win_stopwatch.h
@@ -0,0 +1,7 @@
+#ifndef win_stopwatch_H_INCLUDED   
+#define win_stopwatch_H_INCLUDED   
+ 
+void start_win_stopwatch();  
+void end_win_stopwatch();  
+double get_win_stopwatch_ms();  
+#endif // win_stopwatch_H_INCLUDED 
